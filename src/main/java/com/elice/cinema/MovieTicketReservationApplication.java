f59fc68acba7093e935eac07ffc83b1fa0d6c7ac
user <whddnjs039@naver.com>
2026-01-14 21:05:04 +0900

    init

---
+++ src/main/java/com/elice/cinema/MovieTicketReservationApplication.java
@@ -0,0 +1,13 @@
+package com.elice.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTicketReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elice/cinema/MovieTicketReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
