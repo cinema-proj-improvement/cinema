146a8fcc9f38bd5b732e05e9e33297671fc18612
[CLOUD6] 박윤수 <aspv12@nate.com>
2026-01-24 16:57:28 +0900

    feat(movie): 영화 엔티티 및 상태/관람등급 enum 추가

---
+++ src/main/java/com/elice/cinema/domain/movie/entity/AgeRating.java
@@ -0,0 +1,9 @@
+package com.elice.cinema.domain.movie.entity;
+
+public enum AgeRating {
+
+    ALL,
+    AGE_12,
+    AGE_15,
+    AGE_19
+}
---
+++ src/main/java/com/elice/cinema/domain/movie/entity/MovieStatus.java
@@ -0,0 +1,8 @@
+package com.elice.cinema.domain.movie.entity;
+
+public enum MovieStatus {
+
+    UPCOMING,
+    NOW_SHOWING,
+    ENDED
+}
