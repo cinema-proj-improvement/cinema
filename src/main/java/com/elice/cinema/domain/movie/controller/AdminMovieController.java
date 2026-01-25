9aff655eb73a929518be284a392fb15e6e4bb490
[CLOUD6] 박윤수 <aspv12@nate.com>
2026-01-26 00:49:33 +0900

    [MOV-R-03] feat(movie): 관리자 영화 검색 조회 기능 추가 - 관리자 영화 목록에서 제목 기준 검색 기능 추가 - 페이지네이션 기반 목록 조회에 검색 조건 적용 - 기존 영화 목록 조회 흐름 확장

--- src/main/java/com/elice/cinema/domain/movie/controller/AdminMovieController.java
+++ src/main/java/com/elice/cinema/domain/movie/controller/AdminMovieController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin/movies")
